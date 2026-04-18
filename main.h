b9e55677e0b243d2f77b6cf7e0f39ad7c5d3ccde
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-04-18 20:22:38 +0300

    main header file

--- main.h
+++ main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 #include <stdarg.h>
-int _putchar(char c);
+
 int _printf(const char *format, ...);
+int handle_specifier(char c, va_list args);
+int print_char(va_list args);
+int print_string(va_list args);
 int print_number(int n);
+int _putchar(char c);
 
 #endif
